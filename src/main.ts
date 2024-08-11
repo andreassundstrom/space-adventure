a05712e903beb7b716911288ea2f05865664eb8e
Andreas Sundström <andreas-sundstrom@hotmail.com>
2024-08-11 18:16:08 +0200

    House movement

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+let targetX = 0;
+let currentX = 0;
+let targetY = 0;
+let currentY = 0;
+let targetZ = 0;
+let currentZ = 0;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("scene")?.appendChild(renderer.domElement)
@@ -13,6 +20,7 @@ document.getElementById("scene")?.appendChild(renderer.domElement)
 camera.position.set(-3,2,-5)
 // Load model
 const loader = new GLTFLoader();
+
 let house : GLTF | null;
 
 loader.load('building/scene.gltf',houseModel => {
@@ -22,10 +30,63 @@ loader.load('building/scene.gltf',houseModel => {
   house.scene.rotation.set(0,-2,0)
 })
 
+function addEventListeners(){
+  document.getElementById("about-box")?.addEventListener("mouseover",() => {
+    // house?.scene.rotation.set(0,-1,0)
+    targetX = 0;
+    targetY = -0.5;
+    targetZ = 0;
+  })
+  document.getElementById("showcase-box")?.addEventListener("mouseover",() => {
+    house?.scene.rotation.set(0,-1.5,0)
+    targetX = 0;
+    targetY = -1.0;
+    targetZ = 0;
+  })
+  document.getElementById("contact-box")?.addEventListener("mouseover",() => {
+    house?.scene.rotation.set(0,-2.0,0)
+    targetX = 0;
+    targetY = -2.0;
+    targetZ = 0;
+  })
+
+  document.getElementById("scene")?.addEventListener("mouseover",() => {
+    house?.scene.rotation.set(0,-2.0,0)
+    targetX = 0;
+    targetY = -1.0;
+    targetZ = 0.0;
+  })
+}
+
+addEventListeners()
+
+const speed = 0.1
 function animate(){
-  if(house){
-    house.scene.rotateY(0.0001)
+  
+  if(currentX < targetX){
+    currentX += speed
   }
+  if(currentX > targetX){
+    currentX -= speed
+  }
+
+  if(currentY < targetY){
+    currentY += speed
+  }
+  if(currentY > targetY){
+    currentY -= speed
+  }
+
+  if(currentZ < targetZ){
+    currentZ += speed
+  }
+  if(currentZ > targetZ){
+    currentZ -= speed
+  }
+
+  house?.scene.rotation.set(currentX,currentY,currentZ)
+
   renderer.render(scene, camera)
 }
+
 renderer.setAnimationLoop(animate)
